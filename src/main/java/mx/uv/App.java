ef3a8258bc4b5ae9dc4ed46c03efb7c2a22b769b
ejemplo <clear>
2022-12-01 12:17:27 -0600

    add port listening

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -34,5 +34,15 @@ public class App {
             usuarios.put(u.getId(), u);
             return "usuario agregado";
         });
+
+        static int getHerokuAssignedPort() {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            if (processBuilder.environment().get("PORT") != null) {
+                return Integer.parseInt(processBuilder.environment().get("PORT"));
+            }
+            return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+        }
     }
+
+
 }
